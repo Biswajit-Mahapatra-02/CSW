eba03def51bf9a366798db03005f6b70b8f4094a
Biswajit Mahapatra <mahapatra.biswajit1102@gmail.com>
2022-11-18 05:41:13 +0000

    Done

--- 18-11-22/app.js
+++ 18-11-22/app.js
@@ -5,3 +5,10 @@ function appTxt() {
     t3.innerHTML = "Some Content.";
     $("body").append(t1, t2, t3);
 }
+function appTxt1() {
+    var t1 = "<p>Using HTML to generate a paragraph with <strong>a strong element in it</strong></p>";
+    var t2 = $("<h3></h3>").text("Using jQuery to generate a heading3.");
+    var t3 = document.createElement("article");
+    t3.innerHTML = "Some Article Content.";
+    $("body").prepend(t1, t2, t3);
+}
